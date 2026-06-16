e34125916b783ce129288e72a453daf8c6ee58bc
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-06-16 09:30:29 +0300

    feat: add pagination config

---
+++ config/PaginationConfig.php
@@ -0,0 +1,46 @@
+<?php
+// ============================================================================
+// File:    PaginationConfig.php
+// Author:  Recep Seymen Konuk <konukrecepseymen@gmail.com>
+//
+// Licensed under the terms of the LICENSE file in the project root directory.
+// ============================================================================
+
+namespace Config;
+
+
+class PaginationConfig
+{
+    const VIDEO_PER_PAGE = 30;
+    const SHORT_PER_PAGE = 45;
+    const MUSIC_PER_PAGE = 36;
+
+    const CATEGORY_PER_PAGE = 30;
+    const PLAYLIST_PER_PAGE = 30;
+
+    const CATEGORY_CONTENT_PER_PAGE = 20;
+    const PLAYLIST_CONTENT_PER_PAGE = 20;
+    const LIKED_CONTENT_PER_PAGE = 20;
+    const HISTORY_CONTENT_PER_PAGE = 20;
+    const WATCH_LATER_CONTENT_PER_PAGE = 20;
+
+    const FEED_CONTENT_PER_PAGE = 99;
+    const FEED_SUBSCRIPTION_CONTENT_PER_PAGE = 20;
+    const FEED_CHANNEL_PER_PAGE = 8;
+    const FEED_COMMENT_PER_PAGE = 99;
+    const FEED_PLAYLIST_PER_PAGE = 30;
+
+    const CHANNEL_PER_PAGE = 8;
+
+    const CHANNEL_VIDEO_PER_PAGE = 30;
+    const CHANNEL_SHORT_PER_PAGE = 45;
+    const CHANNEL_MUSIC_PER_PAGE = 36;
+    const CHANNEL_CHANNEL_PER_PAGE = 8;
+    const CHANNEL_PLAYLIST_PER_PAGE = 30;
+
+    const STUDIO_VIDEO_PER_PAGE = 20;
+    const STUDIO_SHORT_PER_PAGE = 20;
+    const STUDIO_MUSIC_PER_PAGE = 20;
+    const STUDIO_CHANNEL_PER_PAGE = 8;
+    const STUDIO_PLAYLIST_PER_PAGE = 20;
+}
