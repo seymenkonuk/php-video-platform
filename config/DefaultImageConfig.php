f083e22e65cf5c250903ab6d7f4498be0dc73d58
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-06-16 09:30:48 +0300

    feat: add default image config

---
+++ config/DefaultImageConfig.php
@@ -0,0 +1,23 @@
+<?php
+// ============================================================================
+// File:    DefaultImageConfig.php
+// Author:  Recep Seymen Konuk <konukrecepseymen@gmail.com>
+//
+// Licensed under the terms of the LICENSE file in the project root directory.
+// ============================================================================
+
+namespace Config;
+
+
+class DefaultImageConfig
+{
+    const DEFAULT_CHANNEL_AVATAR = "/static/defaults/channels/default-avatar.png";
+    const DEFAULT_CHANNEL_BANNER = "/static/defaults/channels/default-banner.png";
+
+    const DEFAULT_CATEGORY_BANNER = "/static/defaults/categories/default.png";
+    const DEFAULT_PLAYLIST_BANNER = "/static/defaults/playlists/default.png";
+
+    const DEFAULT_VIDEO_THUMBNAIL = "/static/defaults/videos/default.png";
+    const DEFAULT_SHORT_THUMBNAIL = "/static/defaults/shorts/default.png";
+    const DEFAULT_MUSIC_THUMBNAIL = "/static/defaults/musics/default.png";
+}
