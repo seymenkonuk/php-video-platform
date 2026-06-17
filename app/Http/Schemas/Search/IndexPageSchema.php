216ebfc120362d7b4b616bed7e344ba0f6490a8d
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-06-17 22:01:59 +0300

    feat(schemas): implement search schemas

--- app/Http/Schemas/Search/IndexPageSchema.php
+++ app/Http/Schemas/Search/IndexPageSchema.php
@@ -9,6 +9,8 @@
 namespace App\Http\Schemas\Search;
 
 
+use Config\ValidationConfig;
+
 use Seymenkonuk\Framework\Schema;
 
 use Seymenkonuk\Validator\Validator\ObjectValidator;
@@ -16,23 +18,27 @@ use Seymenkonuk\Validator\Validator\ObjectValidator;
 
 class IndexPageSchema extends Schema
 {
-    public function body(): ObjectValidator
-    {
-        return $this->validator->object()->schema([]);
-    }
-
     public function query(): ObjectValidator
     {
-        return $this->validator->object()->schema([]);
-    }
-
-    public function params(): ObjectValidator
-    {
-        return $this->validator->object()->schema([]);
-    }
-
-    public function files(): ObjectValidator
-    {
-        return $this->validator->object()->schema([]);
+        return $this->validator->object()->schema([
+            "q" => $this->validator->field()
+                ->string(),
+            "category" => $this->validator->field()
+                ->string(),
+            "channel" => $this->validator->field()
+                ->string(),
+            "type" => $this->validator->field()
+                ->in(ValidationConfig::CONTENT_TYPE_FILTERS),
+            "duration" => $this->validator->field()
+                ->in(ValidationConfig::DURATION_FILTERS),
+            "sort" => $this->validator->field()
+                ->in(ValidationConfig::SORT_FILTERS),
+            "date" => $this->validator->field()
+                ->in(ValidationConfig::DATE_FILTERS),
+            "page" => $this->validator->field()
+                ->int(false)
+                ->min(1)
+                ->default(1),
+        ]);
     }
 }
