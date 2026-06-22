98ccb034f60d1495988220728323ad9dcc0f7416
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-06-22 18:00:10 +0300

    feat(schemas): implement studio music schemas

--- app/Http/Schemas/Studio/Music/CreateSchema.php
+++ app/Http/Schemas/Studio/Music/CreateSchema.php
@@ -9,6 +9,10 @@
 namespace App\Http\Schemas\Studio\Music;
 
 
+use Config\ValidationConfig;
+
+use App\Support\Helpers\FileValidationHelper;
+
 use Seymenkonuk\Framework\Schema;
 
 use Seymenkonuk\Validator\Validator\ObjectValidator;
@@ -18,21 +22,55 @@ class CreateSchema extends Schema
 {
     public function body(): ObjectValidator
     {
-        return $this->validator->object()->schema([]);
-    }
-
-    public function query(): ObjectValidator
-    {
-        return $this->validator->object()->schema([]);
+        return $this->validator->object()->schema([
+            "title" => $this->validator->field()
+                ->string()
+                ->min(ValidationConfig::TITLE_MIN_LEN)
+                ->max(ValidationConfig::TITLE_MAX_LEN)
+                ->regex(ValidationConfig::TITLE_REGEX_RULE, ValidationConfig::TITLE_REGEX_ERROR)
+                ->required(),
+            "description" => $this->validator->field()
+                ->string()
+                ->min(ValidationConfig::DESC_MIN_LEN)
+                ->max(ValidationConfig::DESC_MAX_LEN)
+                ->regex(ValidationConfig::DESC_REGEX_RULE, ValidationConfig::DESC_REGEX_ERROR),
+            "viewType" => $this->validator->field()
+                ->in(ValidationConfig::VIEW_TYPES)
+                ->required(),
+            "commentType" => $this->validator->field()
+                ->in(ValidationConfig::COMMENT_TYPES)
+                ->required(),
+            "csrfToken" => $this->validator->field()
+                ->string()
+                ->required(),
+        ]);
     }
 
     public function params(): ObjectValidator
     {
-        return $this->validator->object()->schema([]);
+        return $this->validator->object()->schema([
+            "musicCode" => $this->validator->field()
+                ->string()
+                ->required(),
+        ]);
     }
 
     public function files(): ObjectValidator
     {
-        return $this->validator->object()->schema([]);
+        return $this->validator->object()->schema([
+            "file" => FileValidationHelper::config($this->validator)
+                ->mimes(ValidationConfig::ALLOWED_MUSIC_MIME_TYPES)
+                ->extension(ValidationConfig::ALLOWED_MUSIC_EXTENSIONS)
+                ->min(ValidationConfig::MUSIC_MIN_FILE_SIZE)
+                ->max(ValidationConfig::MUSIC_MAX_FILE_SIZE)
+                ->make()
+                ->required(),
+            "thumbnail" => FileValidationHelper::config($this->validator)
+                ->mimes(ValidationConfig::ALLOWED_THUMBNAIL_MIME_TYPES)
+                ->extension(ValidationConfig::ALLOWED_THUMBNAIL_EXTENSIONS)
+                ->min(ValidationConfig::THUMBNAIL_MIN_FILE_SIZE)
+                ->max(ValidationConfig::THUMBNAIL_MAX_FILE_SIZE)
+                ->make(),
+        ]);
     }
 }
