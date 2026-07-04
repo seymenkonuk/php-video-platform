ae40e1ab25cb9000e8204d6c310283c4c19b9bf9
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-07-04 09:46:12 +0300

    feat(DTO): add OptionDTO

---
+++ app/Support/DTOs/OptionDTO.php
@@ -0,0 +1,18 @@
+<?php
+// ============================================================================
+// File:    OptionDTO.php
+// Author:  Recep Seymen Konuk <konukrecepseymen@gmail.com>
+//
+// Licensed under the terms of the LICENSE file in the project root directory.
+// ============================================================================
+
+namespace App\Support\DTOs;
+
+
+readonly class OptionDTO
+{
+    public function __construct(
+        public string $value,
+        public string $title,
+    ) {}
+}
