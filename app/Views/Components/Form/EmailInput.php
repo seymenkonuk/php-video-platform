d873195dbcccb8ffa543a0ed07f8fc6240ee65ae
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-07-05 12:26:41 +0300

    feat(ui): add shortcut form elements

---
+++ app/Views/Components/Form/EmailInput.php
@@ -0,0 +1,49 @@
+<!-- PARAMETERS -->
+<?php
+
+/** @var string $name  */
+/** @var ?string $label  */
+/** @var ?string $icon  */
+/** @var string $placeholder  */
+/** @var ?string $description  */
+/** @var ?string|array<string> $errors  */
+/** @var ?string $value  */
+/** @var ?bool $required  */
+/** @var ?bool $disabled  */
+/** @var ?string $autocomplete  */
+/** @var ?string $min  */
+/** @var ?string $max  */
+/** @var ?string $step  */
+
+?>
+
+<!-- DEFAULT VALUE -->
+<?php
+
+$label ??= "";
+$icon ??= "";
+$description ??= "";
+$errors ??= "";
+$value ??= "";
+$required ??= false;
+$disabled ??= false;
+
+?>
+
+<!-- CONTENT -->
+<?= $this->insert("Components/Form/Input", [
+    "name" => $name,
+    "type" => "email",
+    "label" => $label,
+    "icon" => $icon,
+    "placeholder" => $placeholder,
+    "description" => $description,
+    "errors" => $errors,
+    "value" => $value,
+    "required" => $required,
+    "disabled" => $disabled,
+    "autocomplete" => $autocomplete,
+    "min" => $min,
+    "max" => $max,
+    "step" => $step,
+]) ?>
---
+++ app/Views/Components/Form/Submit.php
@@ -0,0 +1,35 @@
+<!-- PARAMETERS -->
+<?php
+
+/** @var ?string $id  */
+/** @var ?string $icon  */
+/** @var string $text  */
+/** @var string $color  */
+/** @var string $hoverColor  */
+/** @var string $textColor  */
+/** @var ?bool $fullWidth  */
+/** @var ?bool $disabled  */
+
+?>
+
+<!-- DEFAULT VALUE -->
+<?php
+
+$id ??= "";
+$icon ??= "";
+$fullWidth ??= true;
+$disabled ??= false;
+
+?>
+
+<!-- CONTENT -->
+<?= $this->insert("Components/Form/Button", [
+    "id" => $id,
+    "icon" => $icon,
+    "text" => $text,
+    "color" => $color,
+    "hoverColor" => $hoverColor,
+    "textColor" => $textColor,
+    "fullWidth" => $fullWidth,
+    "disabled" => $disabled,
+]) ?>
