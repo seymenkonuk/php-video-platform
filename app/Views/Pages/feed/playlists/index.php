269a4f7a531fa27339f8d1ee87bf71fa11f04a77
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-07-17 22:23:25 +0300

    feat: add placeholder feed/playlists page

---
+++ app/Views/Pages/feed/playlists/index.php
@@ -0,0 +1,74 @@
+<!-- CONTEXT -->
+<?php /** @var \League\Plates\Template\Template $this */ ?>
+
+<!-- PARAMETERS -->
+<?php
+/** @var \App\Support\ViewModels\Feed\PlaylistsPageViewModel $model  */
+?>
+
+<!-- EXTRACT MODEL DATA -->
+<?php
+$playlists = $model->playlists;
+$pagination = $model->pagination;
+$brandName = $model->brandName;
+$csrfToken = $model->csrfToken;
+$dateYear = $model->dateYear;
+$navMenus = $model->navMenus;
+$auth = $model->auth;
+?>
+
+<!-- LAYOUT -->
+<?= $this->layout("Layouts/App", [
+    "brandName" => $brandName,
+    "title" => "Oynatma Listelerin",
+    "description" => "",
+    "csrfToken" => $csrfToken,
+    "activeNav" => "/feed/playlists",
+    "navMenus" => $navMenus,
+    "dateYear" => $dateYear,
+    "auth" => $auth,
+]) ?>
+
+<!-- CONTENT -->
+<!-- Sayfa Başlığı -->
+<?= $this->insert("Components/Common/PageHeader", [
+    "icon" => "bi-collection",
+    "title" => "Oynatma Listelerin",
+    "description" => "Oluşturduğun oynatma listeleri.",
+]) ?>
+<!-- Kategoriler -->
+<?php if ($playlists->valid()): ?>
+    <!-- Sonuç Adedi  -->
+    <?= $this->insert("Components/Common/ResultCount", [
+        "count" => $pagination->total,
+        "name" => "oynatma listesi",
+    ]); ?>
+    <!-- Sonuçlar -->
+    <section class="grid grid-cols-1 gap-5 md:grid-cols-2 2xl:grid-cols-3">
+        <?php foreach ($playlists as $playlist): ?>
+            <?= $this->insert("Components/Playlist/Card", [
+                "playlist" => $playlist,
+            ]); ?>
+        <?php endforeach ?>
+    </section>
+<?php else: ?>
+    <?= $this->insert("Components/Common/EmptyState", [
+        "icon" => 'bi-collection-play',
+        "title" => 'Henüz oynatma listesi yok',
+        "description" => 'Henüz hiç oynatma listesi oluşturmadınız',
+    ]) ?>
+<?php endif ?>
+<!-- Sayfalama -->
+<section class="grid grid-cols-1 gap-4">
+    <?= $this->insert("Components/Common/Pagination", [
+        "pagination" => $pagination,
+    ]); ?>
+</section>
+
+<!-- SCRIPT -->
+<?= $this->start("scripts") ?>
+<?= $this->stop() ?>
+
+<!-- STYLE -->
+<?= $this->start("styles") ?>
+<?= $this->stop() ?>
