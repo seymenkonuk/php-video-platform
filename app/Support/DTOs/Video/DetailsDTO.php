153be609ddc184cd8c495a29ed63534c42ab79a6
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-07-19 22:11:43 +0300

    feat(video): add video details DTO

---
+++ app/Support/DTOs/Video/DetailsDTO.php
@@ -0,0 +1,36 @@
+<?php
+// ============================================================================
+// File:    DetailsDTO.php
+// Author:  Recep Seymen Konuk <konukrecepseymen@gmail.com>
+//
+// Licensed under the terms of the LICENSE file in the project root directory.
+// ============================================================================
+
+namespace App\Support\DTOs\Video;
+
+
+use App\Support\DTOs\Channel\DetailsDTO as ChannelDetailsDTO;
+
+
+readonly class DetailsDTO
+{
+    public function __construct(
+        public string               $url,
+        public string               $title,
+        public ?string              $description,
+        public string               $thumbnail,
+        public string               $sourceUrl,
+        public ChannelDetailsDTO    $channel,
+        public int                  $viewCount,
+        public string               $viewCountFormatted,
+        public string               $date,
+        public string               $dateFormatted,
+        public bool                 $liked,
+        public int                  $likeCount,
+        public string               $likeCountFormatted,
+        public bool                 $disliked,
+        public int                  $dislikeCount,
+        public string               $dislikeCountFormatted,
+        public bool                 $watchLater,
+    ) {}
+}
