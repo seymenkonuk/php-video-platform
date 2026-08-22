e7933993c8456cfcb7341b20fed092b8d1e4e86b
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-08-22 23:37:56 +0300

    refactor(helper): support file paths

--- app/Support/Helpers/PathHelper.php
+++ app/Support/Helpers/PathHelper.php
@@ -11,48 +11,83 @@ namespace App\Support\Helpers;
 
 class PathHelper
 {
-    public function storage(): string
+    public function storage(?string $filename = null): string
     {
-        return dirname(__DIR__, 3) . DIRECTORY_SEPARATOR . "storage";
+        return $this->path(
+            dirname(__DIR__, 3) . DIRECTORY_SEPARATOR . "storage",
+            $filename,
+        );
     }
 
-    public function cache(): string
+    public function cache(?string $filename = null): string
     {
-        return $this->storage() . DIRECTORY_SEPARATOR . "cache";
+        return $this->path(
+            $this->storage() . DIRECTORY_SEPARATOR . "cache",
+            $filename,
+        );
     }
 
-    public function logs(): string
+    public function logs(?string $filename = null): string
     {
-        return $this->storage() . DIRECTORY_SEPARATOR . "logs";
+        return $this->path(
+            $this->storage() . DIRECTORY_SEPARATOR . "logs",
+            $filename,
+        );
     }
 
-    public function sessions(): string
+    public function sessions(?string $filename = null): string
     {
-        return $this->storage() . DIRECTORY_SEPARATOR . "sessions";
+        return $this->path(
+            $this->storage() . DIRECTORY_SEPARATOR . "sessions",
+            $filename,
+        );
     }
 
-    public function temp(): string
+    public function temp(?string $filename = null): string
     {
-        return $this->storage() . DIRECTORY_SEPARATOR . "temp";
+        return $this->path(
+            $this->storage() . DIRECTORY_SEPARATOR . "temp",
+            $filename,
+        );
     }
 
-    public function uploads(): string
+    public function uploads(?string $filename = null): string
     {
-        return $this->storage() . DIRECTORY_SEPARATOR . "uploads";
+        return $this->path(
+            $this->storage() . DIRECTORY_SEPARATOR . "uploads",
+            $filename,
+        );
     }
 
-    public function app(): string
+    public function app(?string $filename = null): string
     {
-        return $this->storage() . DIRECTORY_SEPARATOR . "app";
+        return $this->path(
+            $this->storage() . DIRECTORY_SEPARATOR . "app",
+            $filename,
+        );
     }
 
-    public function public(): string
+    public function public(?string $filename = null): string
     {
-        return $this->app() . DIRECTORY_SEPARATOR . "public";
+        return $this->path(
+            $this->app() . DIRECTORY_SEPARATOR . "public",
+            $filename,
+        );
     }
 
-    public function private(): string
+    public function private(?string $filename = null): string
     {
-        return $this->app() . DIRECTORY_SEPARATOR . "private";
+        return $this->path(
+            $this->app() . DIRECTORY_SEPARATOR . "private",
+            $filename,
+        );
+    }
+
+    private function path(string $directory, ?string $filename = null): string
+    {
+        if ($filename !== null) {
+            return $directory . DIRECTORY_SEPARATOR . $filename;
+        }
+        return $directory;
     }
 }
