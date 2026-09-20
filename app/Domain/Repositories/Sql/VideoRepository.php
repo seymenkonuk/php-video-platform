add9feef12686a92560e2f2c7072cbe1e6666bf4
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-09-20 23:35:33 +0300

    feat(repository): implement VideoRepository

--- app/Domain/Repositories/Sql/VideoRepository.php
+++ app/Domain/Repositories/Sql/VideoRepository.php
@@ -14,8 +14,10 @@ use Generator;
 use Seymenkonuk\Framework\Database\SqlRepository;
 
 use App\Domain\Enums\VideoType;
+use App\Domain\Enums\ViewType;
 use App\Domain\Models\Video;
 use App\Domain\Models\VideoDetails;
+use App\Domain\Models\VideoWithChannel;
 use App\Domain\Repositories\Abstract\IVideoRepository;
 
 
@@ -38,12 +40,34 @@ class VideoRepository extends SqlRepository implements IVideoRepository
 
     public function countPublic(): int
     {
-        throw new \Exception('Not implemented');
+        $publicViewType = ViewType::PUBLIC->value;
+        /** @var int */
+        $value = $this->database
+            ->query("
+                SELECT COUNT(*)
+                FROM {$this->table}
+                WHERE video_type = {$this->type}
+                  AND view_type = $publicViewType
+            ")
+            ->execute()
+            ->column();
+        return $value;
     }
 
     public function yieldPublic(int $offset, int $limit): Generator
     {
-        throw new \Exception('Not implemented');
+        $publicViewType = ViewType::PUBLIC->value;
+        return $this->database
+            ->query("
+                SELECT v.*, c.code channel_code, c.title channel_title, c.avatar_path channel_avatar
+                FROM {$this->table} v, channel c
+                WHERE v.uploader_id = c.id
+                  AND v.video_type = {$this->type}
+                  AND v.view_type = $publicViewType
+                LIMIT $offset, $limit
+            ")
+            ->execute()
+            ->cursor(VideoWithChannel::class);
     }
 
     // --------------------------------------------------------------------------
@@ -52,12 +76,37 @@ class VideoRepository extends SqlRepository implements IVideoRepository
 
     public function countPublicByChannel(string $channelCode): int
     {
-        throw new \Exception('Not implemented');
+        $publicViewType = ViewType::PUBLIC->value;
+        /** @var int */
+        $value = $this->database
+            ->query("
+                SELECT COUNT(*)
+                FROM {$this->table} v, channel c
+                WHERE v.uploader_id = c.id
+                  AND v.video_type = {$this->type}
+                  AND v.view_type = $publicViewType
+                  AND c.code = :channelCode
+            ")
+            ->execute(["channelCode" => $channelCode])
+            ->column();
+        return $value;
     }
 
     public function yieldPublicByChannel(string $channelCode, int $offset, int $limit): Generator
     {
-        throw new \Exception('Not implemented');
+        $publicViewType = ViewType::PUBLIC->value;
+        return $this->database
+            ->query("
+                SELECT v.*, c.code channel_code, c.title channel_title, c.avatar_path channel_avatar
+                FROM {$this->table} v, channel c
+                WHERE v.uploader_id = c.id
+                  AND v.video_type = {$this->type}
+                  AND v.view_type = $publicViewType
+                  AND c.code = :channelCode
+                LIMIT $offset, $limit
+            ")
+            ->execute(["channelCode" => $channelCode])
+            ->cursor(VideoWithChannel::class);
     }
 
     // --------------------------------------------------------------------------
@@ -66,12 +115,33 @@ class VideoRepository extends SqlRepository implements IVideoRepository
 
     public function countByChannel(string $channelCode): int
     {
-        throw new \Exception('Not implemented');
+        /** @var int */
+        $value = $this->database
+            ->query("
+                SELECT COUNT(*)
+                FROM {$this->table} v, channel c
+                WHERE v.uploader_id = c.id
+                  AND v.video_type = {$this->type}
+                  AND c.code = :channelCode
+            ")
+            ->execute(["channelCode" => $channelCode])
+            ->column();
+        return $value;
     }
 
     public function yieldByChannel(string $channelCode, int $offset, int $limit): Generator
     {
-        throw new \Exception('Not implemented');
+        return $this->database
+            ->query("
+                SELECT v.*, c.code channel_code, c.title channel_title, c.avatar_path channel_avatar
+                FROM {$this->table} v, channel c
+                WHERE v.uploader_id = c.id
+                  AND v.video_type = {$this->type}
+                  AND c.code = :channelCode
+                LIMIT $offset, $limit
+            ")
+            ->execute(["channelCode" => $channelCode])
+            ->cursor(VideoWithChannel::class);
     }
 
     // --------------------------------------------------------------------------
@@ -84,13 +154,11 @@ class VideoRepository extends SqlRepository implements IVideoRepository
             ->query("
                 SELECT *
                 FROM {$this->table}
-                WHERE code = :code AND video_type = :type
+                WHERE code = :code 
+                  AND video_type = {$this->type}
                 LIMIT 1
             ")
-            ->execute([
-                "code" => $code,
-                "type" => $this->type,
-            ])
+            ->execute(["code" => $code])
             ->fetch($this->model);
     }
 
@@ -111,11 +179,23 @@ class VideoRepository extends SqlRepository implements IVideoRepository
 
     public function update(int|string $code, array $video): bool
     {
+        // Bu koda sahip video yok! (belki müzik veya kısa video olabilir)
+        if (!$this->findByCode((string)$code)) {
+            return false;
+        }
+        // Yukarıdaki kontrol yanlışlıkla istenen türdeki içerik yerine başka içeriğin değiştirilmesini engelliyor1
+        // Örneğin $this->videoRepository->update("example-code", ...) müzik içeriğini güncelleyebilir.
         return parent::update($code, $video);
     }
 
     public function delete(int|string $code): bool
     {
+        // Bu koda sahip video yok! (belki müzik veya kısa video olabilir)
+        if (!$this->findByCode((string)$code)) {
+            return false;
+        }
+        // Yukarıdaki kontrol yanlışlıkla istenen türdeki içerik yerine başka içeriğin silinmesini engelliyor1
+        // Örneğin $this->videoRepository->delete("example-code") kısa video içeriğini silebilir.
         return parent::delete($code);
     }
 }
